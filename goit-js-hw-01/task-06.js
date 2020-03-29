f1e4e9094fb71e60e5a59969c0ffcb784ce5951a
Dmitry Shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-03-29 19:30:29 +0300

    task 6 done

--- goit-js-hw-01/task-06.js
+++ goit-js-hw-01/task-06.js
@@ -4,20 +4,14 @@
 let message;
 let total = 0;
 let input;
-do {
+while (input !== null) {
   input = prompt('Введите число');
   total += Number(input);
-  console.log(Number.isNaN(total));
-  console.log(input);
-  Number.parseInt(total);
-} while (input !== null);
-message = `Общая сумма чисел равна ${total}`;
-console.log(input);
-// if (Number.isNaN(total) === true) {
-//   message = 'Было введено не число, попробуйте еще раз';
-// }
-
-
-
+  message = `Общая сумма чисел равна ${total}`;
+  if (Number.isNaN(total) === true) {
+    message = 'Было введено не число, попробуйте еще раз';
+    break;
+  }
+}
 
 alert(message);
