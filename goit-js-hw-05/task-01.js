255a1921c4e017fa269891e88e579a7ed465c373
dmitry shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-04-25 20:58:58 +0300

    task 01, 02 done

---
+++ goit-js-hw-05/task-01.js
@@ -0,0 +1,30 @@
+//task-01
+'use strict'
+
+const Account = function ({
+  login,
+  email
+}) {
+  this.login = login;
+  this.email = email;
+}
+
+Account.prototype.getInfo = function () {
+  console.log(`Name: ${this.login}, Email: ${this.email}`);
+}
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
