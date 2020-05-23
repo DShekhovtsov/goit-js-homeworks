3639893155e10c3798a56cd3725cf13499724239
dmitry shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-05-23 21:01:04 +0300

    task 04 done

--- goit-js-hw-07/task-04.js
+++ goit-js-hw-07/task-04.js
@@ -1,2 +1,19 @@
 // task-04
 'use strict';
+
+const incrementButton = document.querySelector('button[data-action="increment"]');
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector('#value');
+
+let counterValue = 0;
+const increment = function () {
+  counterValue += 1;
+  value.textContent = counterValue;
+};
+const decrement = function () {
+  counterValue -= 1;
+  value.textContent = counterValue;
+};
+
+incrementButton.addEventListener('click', increment);
+decrementButton.addEventListener('click', decrement);
