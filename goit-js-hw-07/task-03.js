e1de1988b230216efdaf9497ae28bd101ce907a5
dmitry shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-05-23 17:41:34 +0300

    task 03 done

--- goit-js-hw-07/task-03.js
+++ goit-js-hw-07/task-03.js
@@ -1,2 +1,25 @@
 // task-03
 'use strict';
+
+const images = [{
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryUl = document.querySelector('#gallery');
+const wwe = images.map(image =>
+  `<li><img src="${image.url}" alt="${image.alt}" width="400" height="280"></li>`
+).join('')
+console.log(galleryUl)
+galleryUl.insertAdjacentHTML('beforeend', wwe)
+const headTag = document.querySelector('head');
+headTag.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="styles.css">`)
