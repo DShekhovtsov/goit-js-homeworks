0c930b6dbef7db364bd56d77c96c8741aefcb546
dmitry shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-05-23 21:47:48 +0300

    task-05 done

--- goit-js-hw-07/task-05.js
+++ goit-js-hw-07/task-05.js
@@ -1,2 +1,12 @@
 // task-05
 'use strict';
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+const inputValue = function (event) {
+  nameOutput.textContent = event.currentTarget.value;
+  if (nameOutput.textContent === '') {
+    nameOutput.textContent = 'незнакомец';
+  }
+}
+nameInput.addEventListener('input', inputValue);
