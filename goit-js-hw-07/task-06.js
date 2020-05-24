514311ebf898ac05db3621762b5f5cf959ab11b1
dmitry shekhovtsov <dmitry.shekhovtsov@gmail.com>
2020-05-24 12:53:29 +0300

    task 07 done

--- goit-js-hw-07/task-06.js
+++ goit-js-hw-07/task-06.js
@@ -1,2 +1,15 @@
 // task-06
 'use strict';
+
+const input = document.querySelector('#validation-input');
+const noFocus = function () {
+  const setLength = Number(input.dataset.length);
+  let enteredLength = input.value.length;
+  if (setLength === enteredLength) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.add('invalid');
+  };
+}
+input.addEventListener('blur', noFocus);
--- goit-js-hw-07/task-07.js
+++ goit-js-hw-07/task-07.js
@@ -1,2 +1,10 @@
 // task-07
 'use strict';
+
+const fontSize = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+const changeFontSize = function () {
+  let size = event.currentTarget.value;
+  text.style.fontSize = `${size}px`;
+}
+fontSize.addEventListener('input', changeFontSize);
